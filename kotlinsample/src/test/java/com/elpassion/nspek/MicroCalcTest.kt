4b04c919cb95036a2d65222e09645055487b2b69
ghostbuster91 <kghost0@gmail.com>
2017-12-29 13:12:22 +0100

    Fix wrong import2

--- kotlinsample/src/test/java/com/elpassion/nspek/MicroCalcTest.kt
+++ kotlinsample/src/test/java/com/elpassion/nspek/MicroCalcTest.kt
@@ -1,57 +1,56 @@
 package com.elpassion.nspek
 
-import com.elpassion.mspek.MiniSpek.mspek
-import com.elpassion.mspek.MiniSpek.o
-import com.elpassion.nspek.eq
+import org.junit.Assert
 import org.junit.runner.RunWith
 
-@RunWith(MiniSpekRunner::class)
+@RunWith(NSpekRunner::class)
 class MicroCalcTest {
 
-    init {
-        mspek("MicroCalc tests") {
+    fun NSpekMethodContext.`MicroCalc tests`() {
+        "create SUT" o {
 
-            "create SUT" o {
+            val sut = MicroCalc(10)
 
-                val sut = MicroCalc(10)
+            "check add" o {
+                sut.add(5)
+                sut.result eq 15
+                sut.add(100)
+                sut.result eq 115
+            }
 
-                "check add" o {
-                    sut.add(5)
-                    sut.result eq 15
-                    sut.add(100)
-                    sut.result eq 115
-                }
-
-                "mutate SUT" o {
-                    sut.add(1)
+            "mutate SUT" o {
+                sut.add(1)
 
 //                    "incorrectly check add - this should fail" o {
 //                        sut.add(5)
 //                        sut.result eq 15
 //                    }
-                }
-
-                "check add again" o {
-                    sut.add(5)
-                    sut.result eq 15
-                    sut.add(100)
-                    sut.result eq 115
-                }
-
-                "mutate SUT and check multiplyBy" o {
-                    sut.result = 3
-
-                    sut.multiplyBy(3)
-                    sut.result eq 9
-                    sut.multiplyBy(4)
-                    sut.result eq 36
-                }
-
-                "assure that SUT is intact by any of sub tests above" o {
-                    sut.result eq 10
-                }
             }
 
+            "check add again" o {
+                sut.add(5)
+                sut.result eq 15
+                sut.add(100)
+                sut.result eq 115
+            }
+
+            "mutate SUT and check multiplyBy" o {
+                sut.result = 3
+
+                sut.multiplyBy(3)
+                sut.result eq 9
+                sut.multiplyBy(4)
+                sut.result eq 36
+            }
+
+            "assure that SUT is intact by any of sub tests above" o {
+                sut.result eq 10
+            }
         }
+
     }
+}
+
+private infix fun Int.eq(i: Int) {
+    Assert.assertEquals(i, this)
 }
